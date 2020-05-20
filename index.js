60e99112a0e53d930723d012635ab41711d14da7
unknown <you@example.com>
2020-05-20 11:27:36 +0100

    made

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 var parser = require('body-parser');
+const heroes = require('superheroes');
+const villains =  require('supervillains');
 
 app.use(parser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/'));
@@ -9,6 +11,21 @@ app.use(express.static(__dirname + '/'));
 // http://localhost:3000 /
 app.get('/', function(req, res){
   res.sendFile(__dirname + "/source/index.html");
+});
+
+app.get('/heroes', function(req, res){
+  res.send("<h1>" + heroes.random());
+});
+
+app.get('/villains', function(req, res){
+  res.send("<h1>" + villains.random());
+})
+app.post('/', function(req, res){
+  var fullname = req.body.personname;
+  res.write("<h1> Welcome  " + fullname);
+  res.write("<br> <h1> your saviour is " + heroes.random());
+  res.write("<br><br> Please stay away from " + villains.random());
+  res.send();
 });
 
 app.get('/about', function(req, res){
